931370ccea85e42a79948ce1dcc9a95ef675ca9b
Adam Blake <kaelambda@gmail.com>
2019-11-11 00:10:49 -0500

    Saving scrollPosition so that we can keep scrolling from where we were when we selected an item.

--- app/src/main/java/com/damakable/kotlinnews/screens/feed/NewsfeedFragment.kt
+++ app/src/main/java/com/damakable/kotlinnews/screens/feed/NewsfeedFragment.kt
@@ -12,9 +12,12 @@ import com.damakable.kotlinnews.model.NewsItem
 import com.damakable.kotlinnews.model.NewsfeedProvider
 import kotlinx.android.synthetic.main.fragment_newsfeed.*
 
+
 class NewsfeedFragment : Fragment(R.layout.fragment_newsfeed), NewsfeedView {
     private lateinit var adapter: NewsfeedAdapter
     private lateinit var presenter: NewsfeedPresenter
+
+    private var scrollPosition = 0
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -40,7 +43,14 @@ class NewsfeedFragment : Fragment(R.layout.fragment_newsfeed), NewsfeedView {
         )
         newsfeed_recycler.addOnScrollListener(EndlessScrollListener(layoutManager, presenter))
 
+        layoutManager.scrollToPosition(scrollPosition)
         presenter.requestFeedIfEmpty()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        val manager = newsfeed_recycler.layoutManager as LinearLayoutManager
+        scrollPosition = manager.findFirstVisibleItemPosition()
     }
 
     override fun onResume() {
